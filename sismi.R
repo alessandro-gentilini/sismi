b607543cfb7c994a4937b73225a86afd6ccc22b8
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2016-08-25 20:55:18 +0200

    First good result.

--- sismi.R
+++ sismi.R
@@ -1,15 +1,21 @@
-# url <- "http://emidius.mi.ingv.it/CPTI15-DBMI15/data/CPTI15_v1.5.ods"
-# 
-# 
-# library(readODS)
-# 
-# download.file(url,'CPTI15_v1.5.ods')
-# 
-# df <- read.ods('CPTI15_v1.5.ods')
-
 url <- "http://emidius.mi.ingv.it/CPTI15-DBMI15/data/CPTI15_v1.5.xls"
 local_file <- 'CPTI15_v1.5.xls'
-download.file(url,local_file)
 
+if(!file.exists(local_file)){
+  download.file(url,local_file)
+}
+
+# http://stackoverflow.com/a/29108475/15485
 library(readxl)
 df <- read_excel(local_file, sheet = "catalogue")
+
+df$date_time <- as.POSIXct(paste(df$Year,df$Mo,df$Da,df$Ho,df$Mi), format="%Y %m %d %H %M")
+df$time <- as.POSIXct(paste(df$Ho,df$Mi), format="%H %M")
+df$MwIns
+
+subset <- data.frame(date_time=df$date_time,magnitudo=df$MwIns,time=df$time)
+subset <- na.omit(subset)
+
+plot(subset$time,subset$magnitudo)
+
+
