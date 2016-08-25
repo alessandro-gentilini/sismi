9860ff099485e15afc25135d6f013c295f9a5193
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2016-08-25 20:36:49 +0200

    readODS get stuck ar read.ods

--- sismi.R
+++ sismi.R
@@ -1,7 +1,15 @@
+# url <- "http://emidius.mi.ingv.it/CPTI15-DBMI15/data/CPTI15_v1.5.ods"
+# 
+# 
+# library(readODS)
+# 
+# download.file(url,'CPTI15_v1.5.ods')
+# 
+# df <- read.ods('CPTI15_v1.5.ods')
+
 url <- "http://emidius.mi.ingv.it/CPTI15-DBMI15/data/CPTI15_v1.5.xls"
+local_file <- 'CPTI15_v1.5.xls'
+download.file(url,local_file)
 
-# require(RCurl)
-# require(gdata)
-# df <- read.xls(url)
-
-df <- read.ods(url)
+library(readxl)
+df <- read_excel(local_file, sheet = "catalogue")
